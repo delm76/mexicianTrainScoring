60c0d412c18aadd6c1a8e7121b6be04999dee1be
delm76 <5407737+delm76@users.noreply.github.com>
2022-04-24 15:15:55 -0400

    Create mexicanTrainScoring.js

---
+++ mexicanTrainScoring.js
@@ -0,0 +1,43 @@
+# Starting as pseudo code.  
+# To be developed as pure javascript.  
+# This is expected to be project to learn git and github collaberation and version control, as well as to improve Javascript skills.
+
+# To be run from console.
+
+# Game Initalialization
+
+print("How many players.")
+
+# Accept number as input and set as variable.
+
+player1 = player1name;
+player2 = player2name;
+player3 = player3name;
+player4 = player4name;
+player5 = player5name;
+
+player1scoreArray = [];
+player2scoreArray = [];
+player3scoreArray = [];
+player4scoreArray = [];
+player5scoreArray = [];
+
+roundNumber = 12; 
+print $roundNumber;
+if (roundNumber > 0):{
+  # implement play score math;
+  pass;
+} else {
+print finalScores;
+exit 0;
+}
+
+# TODO:
+
+# Expand to be a web app.
+
+# Add API.
+
+# Use this as a starting point for implementation for not pure javascript libraries such as React, jquery.
+
+# Later to use with frameworks including AngularJS, Angular, and NodeJS.
